8ec111a8995636d78608f005d7c040335095c584
Omar Mohamed <omarcoptan9@gmail.com>
2025-03-07 01:13:15 +0200

    Initial revert to Xorg

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,34 +1,9 @@
 return {
 	{
-		"AlexvZyl/nordic.nvim",
+		'f4z3r/gruvbox-material.nvim',
+		name = 'gruvbox-material',
 		lazy = false,
 		priority = 1000,
-		config = function()
-			require("nordic").setup({
-				transparent = {
-					bg = true,
-					float = false,
-				},
-				telescope = {
-					style = "classic",
-				},
-				on_highlight = function(highlights, palette)
-					highlights.Comment = {
-						fg = palette.gray5,
-					}
-
-					highlights.StatusLine = {
-						fg = "none",
-					}
-					highlights.LineNr = {
-						fg = palette.gray5,
-					}
-					highlights.PmenuSel = {
-						bg = palette.blue0,
-					}
-				end,
-			})
-			require("nordic").load()
-		end,
+		opts = {},
 	},
 }
