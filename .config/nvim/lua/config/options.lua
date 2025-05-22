8da05457e77b77021d446a9e036c386fd889ab31
Omar Mohamed <omarcoptan9@gmail.com>
2025-05-22 18:47:07 +0300

    nvim: Use case insensitive search

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,6 +11,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
