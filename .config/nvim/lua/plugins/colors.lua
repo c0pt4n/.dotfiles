5665710511ab9040a6ca4d0b4702d312e9ec700f
Omar Mohamed <omarcoptan9@gmail.com>
2025-05-30 18:27:22 +0300

    nvim: welcome transparency
    Change-Id: If6ae628c488a58ed8fa9be3723c2f5ed15ce2255

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,9 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
 			vim.cmd.colorscheme("catppuccin-mocha")
         end
 }
