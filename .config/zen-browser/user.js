98685ae0e8e2ce8d10036281ad58a6f5225eceeb
Omar Mohamed <omarcoptan9@gmail.com>
2025-06-13 19:33:34 +0300

    update zen-browser config
    Change-Id: Ic56d33f365464881619a01cd21a63d6e747f6f8e

--- .config/zen-browser/user.js
+++ .config/zen-browser/user.js
@@ -1,10 +1,11 @@
-user_pref("zen.theme.accent-color", "#3498db");
+user_pref("zen.theme.accent-color", "#aac7ff");
 user_pref("zen.tabs.vertical.right-side", false);
 user_pref("zen.tabs.show-newtab-vertical", false);
 user_pref("zen.tab-unloader.timeout-minutes", 60);
 user_pref("zen.view.show-newtab-button-top", false);
-user_pref("zen.view.compact.hide-toolbar", false);
+user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.view.compact.toolbar-flash-popup", false);
+user_pref("zen.view.use-single-toolbar", false);
 user_pref("zen.theme.color-prefs.use-workspace-colors", true);
 user_pref("zen.splitView.change-on-hover", true);
 user_pref("zen.workspaces.container-specific-essentials-enabled", true);
