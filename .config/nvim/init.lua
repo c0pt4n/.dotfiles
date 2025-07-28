e40065b84a0b224a560eaff09325b33b55b758b1
Omar Mohamed <omarcoptan9@gmail.com>
2025-07-28 15:15:11 +0300

    .config: nvim: clean init.lua
    Change-Id: Ibec562597bbdc427372cdd208f3dd7fea6ebdcde

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,2 @@
-require("config.options")
-require("config.globals")
-require("config.keymaps")
-require("config.autocmd")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", { change_detection = {notify = false} })
-vim.cmd.colorscheme "catppuccin"
+require("config")
+require("plugins")
