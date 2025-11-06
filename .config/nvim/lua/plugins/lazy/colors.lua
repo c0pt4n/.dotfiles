6ea561a1c50a97b9c9320cbc95e39b9c3ed4b7fd
Omar Mohamed <omarcoptan9@gmail.com>
2025-11-07 01:29:06 +0200

    Revert "nordify config"
    This reverts commit 3cc9093f6b6dc9eca096de3821557867cd273325.
    
    Change-Id: I0e0af449ca9bc6e4abff931937044b8a328729d1

--- .config/nvim/lua/plugins/lazy/colors.lua
+++ .config/nvim/lua/plugins/lazy/colors.lua
@@ -1,36 +1,12 @@
 return {
-	{
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
 		priority = 1000,
-		config = function()
-			require("nordic").setup({
-				transparent = {
-					bg = true,
-					float = false,
-				},
-				telescope = {
-					style = "classic",
-				},
-				on_highlight = function(highlights, palette)
-					highlights.Comment = {
-						fg = palette.gray5,
-					}
-					highlights.StatusLine = {
-						fg = "none",
-					}
-					highlights.LineNr = {
-						fg = palette.gray5,
-					}
-					highlights.PmenuSel = {
-						bg = palette.blue0,
-					}
-					highlights.PmenuThumb = {
-						bg = palette.blue0,
-					}
-				end,
+        config = function()
+			require("catppuccin").setup({
+				auto_integrations = true,
+				transparent_background = true,
 			})
-			require("nordic").load({})
-		end,
-	},
+			vim.cmd.colorscheme("catppuccin")
+        end
 }
