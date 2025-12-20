16b943410ca144dd77fd7f426e1b6243c8af91d7
Omar Mohamed <omarcoptan9@gmail.com>
2025-12-21 01:08:08 +0200

    nvim: update lsp config
    Change-Id: Iead59ef1d6045393b13e6b5a3cbb3cb01d89979d

--- .config/nvim/lua/plugins/lazy/lsp.lua
+++ .config/nvim/lua/plugins/lazy/lsp.lua
@@ -1,34 +1,7 @@
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local opts = { buffer = ev.buf, remap = false }
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
-		vim.keymap.set("n", "<leader>af", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "<leader>ak", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
-		vim.keymap.set("n", "<leader>aj", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, opts)
-	end
-})
-
-vim.diagnostic.config({
-	virtual_text = true,
-	update_in_insert = true,
-	float = {
-		border = "rounded",
-		source = true,
-	},
-})
-
 return {
 	{
 		"williamboman/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
+-		event = { "BufReadPre", "BufNewFile" },
 		build = ":MasonUpdate",
 		dependencies = {
 			{ "williamboman/mason.nvim", opts = {}, build = ":MasonUpdate" },
@@ -39,17 +12,53 @@ return {
 			require("mason-lspconfig").setup({
 				automatic_enable = true,
 				ensure_installed = {
-					"clangd",
 					"bashls",
+					"clangd",
+					"efm",
 					"gopls",
-					"pyright",
+					"basedpyright",
+					"ts_ls",
 					"lua_ls",
 					"zk",
 				},
 			})
 
+			vim.diagnostic.config({
+				virtual_text = true,
+				update_in_insert = true,
+				float = {
+					border = "rounded",
+					source = true,
+				},
+			})
+
 			vim.lsp.config("*", {
 				capabilities = vim.lsp.protocol.make_client_capabilities(),
+				on_attach = function(_, bufnr)
+					local opts = { buffer = bufnr, remap = false }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>af", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "<leader>ak", function()
+						vim.diagnostic.jump({ count = 1, float = true })
+					end, opts)
+					vim.keymap.set("n", "<leader>aj", function()
+						vim.diagnostic.jump({ count = -1, float = true })
+					end, opts)
+					vim.keymap.set("n", "<leader>bf", function()
+						require("conform").format({
+							async = true,
+							lsp_format = "fallback",
+						})
+					end, opts)
+					-- vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, opts)
+				end,
 			})
 
 			vim.lsp.config("clangd", {
@@ -77,7 +86,31 @@ return {
 					},
 				},
 			})
+
+			vim.lsp.config("ts_ls", {
+				init_options = {
+					maxTsServerMemory = 3 * 1024,
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vim.fn.expand(
+								"$MASON/packages/vue-language-server/node_modules/@vue/language-server"
+							),
+							languages = { "vue" },
+							configNamespace = "typescript",
+						},
+					},
+				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			})
+
+			vim.lsp.config("arduino_language_server", {
+				cmd = {
+					"arduino-language-server",
+					"-cli-config",
+					vim.fn.expand("$ARDUINO_DIRECTORIES_DATA/arduino-cli.yaml"),
+				},
+			})
 		end,
 	},
 }
-
