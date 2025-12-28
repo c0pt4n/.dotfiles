1bd17d52df805d3c371abed1b2b89f968ad5ccb8
Omar Mohamed <omarcoptan9@gmail.com>
2025-12-28 14:19:59 +0200

    Nordify config
    Change-Id: I47ba491e606b8c68d094dd018fbb724545d6b66a

--- .config/nvim/lua/plugins/lazy/colors.lua
+++ .config/nvim/lua/plugins/lazy/colors.lua
@@ -1,12 +1,36 @@
 return {
-        "catppuccin/nvim",
-        name = "catppuccin",
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
 		priority = 1000,
-        config = function()
-			require("catppuccin").setup({
-				auto_integrations = true,
-				transparent_background = true,
+		config = function()
+			require("nordic").setup({
+				transparent = {
+					bg = true,
+					float = false,
+				},
+				telescope = {
+					style = "classic",
+				},
+				on_highlight = function(highlights, palette)
+					highlights.Comment = {
+						fg = palette.gray5,
+					}
+					highlights.StatusLine = {
+						fg = "none",
+					}
+					highlights.LineNr = {
+						fg = palette.gray5,
+					}
+					highlights.PmenuSel = {
+						bg = palette.blue0,
+					}
+					highlights.PmenuThumb = {
+						bg = palette.blue0,
+					}
+				end,
 			})
-			vim.cmd.colorscheme("catppuccin")
-        end
+			require("nordic").load({})
+		end,
+	},
 }
