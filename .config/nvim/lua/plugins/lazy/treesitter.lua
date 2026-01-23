30be7b2893620112c05aa2c0b063b20195623900
Omar Mohamed <omarcoptan9@gmail.com>
2026-01-23 22:28:58 +0200

    nvim: update treesitter config
    Change-Id: I9e060fdc00be21449c2aaabb785e595441ac29a8

--- .config/nvim/lua/plugins/lazy/treesitter.lua
+++ .config/nvim/lua/plugins/lazy/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter").setup({
 				sync_install = false,
 				auto_install = true,
 				highlight = {
