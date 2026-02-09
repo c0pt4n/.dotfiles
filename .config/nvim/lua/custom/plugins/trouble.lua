2a8627c9a4201e15f6159002765c9a3efde95042
Omar Mohamed <omarcoptan9@gmail.com>
2026-02-09 12:59:33 +0200

    trouble: disable icons
    Change-Id: I3029c87cb020df86b869e5adff8a9ef4020a902c

--- .config/nvim/lua/custom/plugins/trouble.lua
+++ .config/nvim/lua/custom/plugins/trouble.lua
@@ -3,9 +3,11 @@ return {
 		"folke/trouble.nvim",
 		config = function()
 			local trouble = require("trouble")
-			trouble.setup({})
-			vim.keymap.set("n", "<leader>xx", function()
-				trouble.toggle("diagnostics")
+			trouble.setup({
+				icons = false,
+			})
+			vim.keymap.set("n", "<leader>tt", function()
+				trouble.toggle()
 			end)
 			vim.keymap.set("n", "<leader>xl", function()
 				trouble.toggle("loclist")
