ddcbaf7b4a5787b8e5f11b4909a79cef87552332
Omar Mohamed <omarcoptan9@gmail.com>
2026-08-02 15:46:05 +0300

    feat: add initial home-manager config

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,2 +1,19 @@
 {
+  config,
+  custom,
+  ...
+}:
+{
+  imports = [
+    ./config
+  ];
+
+  programs.home-manager.enable = true;
+  home = {
+    username = custom.systemInfo.user;
+    homeDirectory = "/home/${custom.systemInfo.user}";
+    stateVersion = custom.systemInfo.stateVersion;
+    preferXdgDirectories = config.xdg.enable;
+    enableNixpkgsReleaseCheck = true;
+  };
 }
