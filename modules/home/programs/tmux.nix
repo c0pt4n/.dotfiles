44ec4bd23040bdba0187c4e5234e32a9563516d0
Omar Mohamed <omarcoptan9@gmail.com>
2026-08-28 00:33:13 +0300

    feat(tmux): add tmuxx script

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -1,4 +1,10 @@
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
   programs.tmux = {
     enable = true;
     baseIndex = 1;
@@ -32,5 +38,57 @@
       bind -n M-l switch-client -n
       bind -n M-n command-prompt -p "New Session:" "new-session -A -s '%%'"
     '';
+  };
+
+  home.file."${config.xdg.binHome}/tmuxx" = lib.mkIf config.programs.tmux.enable {
+    source =
+      let
+        fzfBin = "${config.programs.fzf.package}/bin/fzf";
+        tmuxBin = "${config.programs.tmux.package}/bin/tmux";
+      in
+      pkgs.writeShellScript "tmuxx" ''
+        set -eu
+
+        PROJECTS_DIR="${config.xdg.userDirs.projects}"
+
+        dirsel() {
+            find "$PROJECTS_DIR" -mindepth 2 -maxdepth 2 -type d -printf "%T@ %P\n" |
+                sort -nr |
+                cut -d " " -f 2- |
+                ${fzfBin}
+        }
+
+        case "''${1:--}" in
+        .)
+            dir="$(pwd)"
+            shift
+            ;;
+        -)
+            dir="$PROJECTS_DIR/$(dirsel)" || exit $?
+            [ $# -gt 0 ] && shift
+            ;;
+        *)
+            dir="$(readlink -f "$1")"
+            shift
+            ;;
+        esac
+
+        [ -d "$dir" ] || {
+            echo "$(basename "$0"): '$dir' is not a directory" >&2
+            exit 1
+        }
+        name="$(basename "$dir")"
+
+        [ $# -gt 1 ] && {
+            ${tmuxBin} new -d -c "$dir" -s "$name" -n "$1"
+            shift
+        }
+
+        for win; do
+            ${tmuxBin} new-window -d -t "$name" -c "$dir" -n "$win"
+        done
+
+        exec ${tmuxBin} new -A -c "$dir" -s "$name"
+      '';
   };
 }
