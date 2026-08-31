3c2d435054cce251ee1f9c29681548f3a1599281
Omar Mohamed <omarcoptan9@gmail.com>
2026-08-31 22:33:52 +0300

    feat(emacs): add emacs-overlay

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -31,9 +31,13 @@
       ];
       use-xdg-base-directories = true;
       auto-optimise-store = true;
-      extra-substituters = [ "https://noctalia.cachix.org" ];
+      extra-substituters = [
+        "https://noctalia.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
       extra-trusted-public-keys = [
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
