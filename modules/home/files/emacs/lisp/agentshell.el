7827b0743a5659e153f1ad6bfa2164a5a648209b
Omar Mohamed <omarcoptan9@gmail.com>
2026-09-20 00:59:50 +0300

    feat(agent-shell): set dot-subdir functions

--- modules/home/files/emacs/lisp/agentshell.el
+++ modules/home/files/emacs/lisp/agentshell.el
@@ -1,6 +1,9 @@
 ;;; agentshell.el --- Agentic workflow -*- lexical-binding: t; -*-
 
-(use-package agent-shell)
+(use-package agent-shell
+  :config
+  (setopt agent-shell-dot-subdir-function
+          (lambda (subdir) (expand-file-name subdir no-littering-var-directory))))
 
 (provide 'agentshell)
 ;;; agentshell.el ends here
