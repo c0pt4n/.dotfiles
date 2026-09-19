a4743e86a2415fb0a847c24b4732fc3c0895156c
Omar Mohamed <omarcoptan9@gmail.com>
2026-09-19 13:41:54 +0300

    fix(emacs): workaround emacsclient theme issue

--- modules/home/files/emacs/lisp/theme.el
+++ modules/home/files/emacs/lisp/theme.el
@@ -1,8 +1,15 @@
 ;;; theme.el --- Nord theme -*- lexical-binding: t; -*-
 
+(defun om/theme-init-daemon ()
+  (load-theme 'nordic-night t)
+  (remove-hook 'server-after-make-frame-hook #'om/theme-init-daemon)
+  (fmakunbound 'om/theme-init-daemon))
+
 (use-package nordic-night-theme
   :init
-  (load-theme 'nordic-night t))
+  (if (daemonp)
+      (add-hook 'server-after-make-frame-hook #'om/theme-init-daemon)
+    (load-theme 'nordic-night t)))
 
 (provide 'theme)
 ;;; theme.el ends here
