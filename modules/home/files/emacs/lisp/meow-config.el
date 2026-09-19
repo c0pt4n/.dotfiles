0182ae60af1d46a1d7b367910d9722b0c3d4cf1d
Omar Mohamed <omarcoptan9@gmail.com>
2026-09-19 17:07:29 +0300

    feat(emacs): add increment / decrement number

--- modules/home/files/emacs/lisp/meow-config.el
+++ modules/home/files/emacs/lisp/meow-config.el
@@ -193,6 +193,23 @@
   (meow-setup)
   (meow-global-mode))
 
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
+(defun decrement-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
+
+(global-set-key (kbd "C-c +") 'increment-number-at-point)
+(global-set-key (kbd "C-c -") 'decrement-number-at-point)
+
 ;; Window movement
 (global-set-key (kbd "C-<left>")  #'windmove-left)
 (global-set-key (kbd "C-<right>") #'windmove-right)
