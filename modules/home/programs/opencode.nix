cf92a9bbc50d46212c989766c4eaf21f42eebb18
Omar Mohamed <omarcoptan9@gmail.com>
2026-09-21 09:44:44 +0300

    feat: update nix flake

--- modules/home/programs/opencode.nix
+++ modules/home/programs/opencode.nix
@@ -2,16 +2,10 @@
   pkgs,
   ...
 }:
-let
-  newPkgs =
-    (builtins.getFlake "github:nixos/nixpkgs/99b76fd9b396189197d2ecce519ab6d7cd522ab5")
-    .legacyPackages.${pkgs.stdenv.hostPlatform.system};
-in
 {
   home.packages = [ pkgs.sqlite ];
   programs.opencode = {
     enable = true;
-    package = newPkgs.opencode;
     enableMcpIntegration = true;
     settings = {
       permission = {
